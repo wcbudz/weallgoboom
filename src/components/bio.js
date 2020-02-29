cb7b01bf0b0760c291e2c3eede0d22c1423b3b54
wcbudz <3577943+wcbudz@users.noreply.github.com>
2020-02-29 13:38:59 -0500

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -59,8 +59,8 @@ const Bio = () => {
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+        <a href={`https://boom.weallgoboom.com`}>
+          Sign up for the weekly digest here
         </a>
       </p>
     </div>
